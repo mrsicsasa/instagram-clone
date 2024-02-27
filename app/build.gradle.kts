b759fbbcdb40b374825846e5657e499f1ff30fae
mrsicsasa <sasamrsic20@gmail.com>
2024-02-27 20:23:02 +0100

    MVVM and hilt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,6 +63,9 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("com.google.firebase:firebase-firestore:24.10.2")
+    implementation ("com.google.firebase:firebase-auth-ktx:21.0.1")
+    implementation("com.google.firebase:firebase-storage:20.3.0")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -74,8 +77,16 @@ dependencies {
     implementation ("androidx.navigation:navigation-compose:2.4.0-beta01")
 
 
-    implementation("com.google.dagger:hilt-android:2.44")
-    kapt("com.google.dagger:hilt-android-compiler:2.44")
+    // Hilt Android dependency
+    implementation ("com.google.dagger:hilt-android:2.40.5")
+    kapt ("com.google.dagger:hilt-android-compiler:2.40.5")
+
+// Hilt ViewModel dependency
+    implementation ("androidx.hilt:hilt-lifecycle-viewmodel:1.0.0-alpha03")
+    kapt ("androidx.hilt:hilt-compiler:1.0.0")
+
+    implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
+
     implementation ("io.coil-kt:coil-compose:1.3.2")
 
     implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.9")
--- app/src/main/java/com/example/instagramclone/MainActivity.kt
+++ app/src/main/java/com/example/instagramclone/MainActivity.kt
@@ -10,8 +10,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.instagramclone.ui.theme.InstagramCloneTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,7 +25,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android app")
+                    InstagramApp()
                 }
             }
         }
@@ -30,17 +33,14 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun InstagramApp(){
+    val vm= hiltViewModel<InstagramViewModel>()
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     InstagramCloneTheme {
-        Greeting("Android")
+        InstagramApp()
     }
 }
